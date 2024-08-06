9f22e13b0e6d1baefc04d872c2cd0cd206d3b38b
NguyenTanPil <traila1008@gmail.com>
2024-08-06 23:09:42 +0700

    Java data types - primitive types

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,52 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // First program
+        System.out.println("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Java Data Types (Primitive)
+        // boolean type - two possible values, either true or false - default false value.
+        boolean flag = true;
+        System.out.println(flag);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // byte type - have values from -128 to 127 (8-bit signed two's complement integer - default 0.
+        byte range = 124;
+        System.out.println(range);
+
+        // short type - have values from -32768 to 32767 (16-bit) it's used instead of other integer data type(int, long) - default 0.
+        short temperature = -300;
+        System.out.println(temperature);
+
+        // int type - can have values from -2^31 to 2^31 - 1 (32-bit) - default 0.
+        int int_range = -425000;
+        System.out.println(int_range);
+
+        // long type - can have values from -2^63 to 2^63 - 1 (64-bit) - default 0.
+        long long_range = -42332200000L;
+        System.out.println(long_range);
+
+        // double type - it's a double precision 64-bit floating-pointer - never used for precise value as currency - default 0.0.
+        double double_number = -23.3;
+        System.out.println(double_number);
+
+        // float type - it's a double precision 32-bit floating-pointer - never used for precise value as currency - default 0.0.
+        float float_number = -42.3f;
+        System.out.println(float_number);
+
+        // char type - it's a 16-bit Unicode character - min is '\u0000'(0) and max is '\uffff' - default '\u0000'
+        char letter = '\u0051';
+        System.out.println(letter);
+
+        char letter1 = '9';
+        System.out.println(letter1);
+
+        char letter2 = 65;
+        System.out.println(letter2);
+
+        // string type - it's not primitive type. It's a object.
+        String myString = "Java Programming";
+        System.out.println(myString);
+
+        // Next lesson: https://www.programiz.com/java-programming/operators
     }
 }
